5a4913b2f680c4cce923b077548772e3a725a0c6
Alex Christian <alexchristianqr@gmail.com>
2023-01-21 10:11:41 -0500

    [added] ghpages

---
+++ ghpages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+   echo "You must specify a new version level: [patch, minor, major]";
+   exit 1;
+fi
+
+tagVersion=$1
+
+set -e
+
+git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
+git push origin "v$tagVersion"
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m "new deployment for release v$tagVersion"
+git push -f git@github.com:alexchristianqr/tres-en-linea.git main:gh-pages
+
+cd -
