83037d359fcded15acffa68c348969a63743ce05
Alex Christian <alexchristianqr@gmail.com>
2023-01-21 23:19:53 -0500

    [updated] home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,14 @@ export class HomeComponent implements OnInit  {
 
   breakpoint: number = 3;
   ngOnInit() {
+    if(window.innerWidth <= 375){
+      this.breakpoint = (window.innerWidth <= 319) ? 1 : 2;
+    }else if(window.innerWidth <= 400){
     this.breakpoint = (window.innerWidth <= 400) ? 1 : 3;
+    }
   }
 
   onResize(event: any) {
-    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
+    this.breakpoint = (event.target.innerWidth <= 319) ? 1 : 6;
   }
 }
