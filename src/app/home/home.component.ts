7fa268de1c43a830ecf445c4519cfb75bd0a8e5b
Alex Christian <alexchristianqr@gmail.com>
2023-01-22 00:59:16 -0500

    [updated] configuraciones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit  {
+export class HomeComponent implements OnInit {
 
   breakpoint: number = 3;
+
   ngOnInit() {
-      this.breakpoint = (window.innerWidth <= 319) ? 1 : 2;
+    this.breakpoint = (window.innerWidth <= 319) ? 1 : 2;
   }
 
   onResize(event: any) {
