df6d85c450095452f9a601ccc61a2c1aaf77f302
Alex Christian <alexchristianqr@gmail.com>
2023-01-30 11:49:44 -0500

    [updated] home apps

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,18 +1,50 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+
+type App = {
+  id: number
+  title: string
+  disabled: boolean
+  icon: string
+  path: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  breakpoint: number = 3
-
-  ngOnInit() {
-    this.breakpoint = window.innerWidth <= 319 ? 1 : 2
-  }
-
-  onResize(event: any) {
-    this.breakpoint = event.target.innerWidth <= 319 ? 1 : 6
+export class HomeComponent {
+  apps: Array<App> = []
+  constructor() {
+    this.apps = [
+      {
+        id: 1,
+        title: 'Tic tac toe',
+        disabled: false,
+        icon: 'gamepad',
+        path: '/game',
+      },
+      {
+        id: 2,
+        title: 'Smart toy',
+        disabled: true,
+        icon: 'smart_toy',
+        path: '/game',
+      },
+      {
+        id: 3,
+        title: 'Basketball',
+        disabled: true,
+        icon: 'sports_volleyball',
+        path: '/game',
+      },
+      {
+        id: 4,
+        title: 'Tetris',
+        disabled: true,
+        icon: 'sports_cricket',
+        path: '/game',
+      },
+    ]
   }
 }
