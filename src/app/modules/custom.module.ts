0af1e70e2a6ae0407e3b6b42111c36dc6a486dca
Alex Christian <alexchristianqr@gmail.com>
2025-01-16 02:06:37 -0500

    Revert "Develop"

--- src/app/modules/custom.module.ts
+++ src/app/modules/custom.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from "@angular/core";
 import { HomeModule } from "./home/home.module";
 import { GameModule } from "./game/game.module";
 import { AuthModule } from "./auth/auth.module";
-import { NgForOf, NgIf } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RouterLink } from "@angular/router";
 
 @NgModule({
-  exports: [HomeModule, GameModule, AuthModule],
-  imports: [NgForOf, NgIf, ReactiveFormsModule, RouterLink]
+  exports: [HomeModule, GameModule, AuthModule]
 })
 export class CustomModule {}
